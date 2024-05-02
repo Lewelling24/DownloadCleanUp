be23da737811ef4f10de90184f3402cbd6b10bd1
Kane Lewelling <kanelewelling@gmail.com>
2024-05-02 16:08:49 -0400

    Add files via upload
    added move file functionality for each type of extension

--- DownloadCleanUp/main.py
+++ DownloadCleanUp/main.py
@@ -1,4 +1,5 @@
 import os # writing for windows
+import shutil # using to move files
 
 #add your file paths here
 downloads_path = ''
@@ -6,10 +7,9 @@ text_file_path = ''
 image_file_path = ''
 audio_file_path = ''
 video_file_path = ''
-executable_file_path = ''
 
 #extensions for various file types
-text_extensions = [".docx", ".txt", ".pdf", ".doc"]
+text_extensions = [".docx", ".txt", ".pdf", ".doc", ".xlsx"]
 
 image_extensions = [".jpg", ".jpeg", ".png", ".gif"]
 
@@ -19,33 +19,35 @@ video_extensions = [".mov", ".avi", ".flv", ".wmv"]
 
 executable_extensions = [".exe", ".jar", ".zip"]
 
-compressed_extensions = [".zip", ".rar", ".tar", ".gz"]
+compressed_extensions = [".zip", ".rar", ".tar", ".gz", ".iso"]
 
 # open the downloads folder
 contents = os.listdir(downloads_path)
 
-# deleting compressed files
+
 for item in contents:
     for i in text_extensions:
         if(item.endswith(i)):
-            os.remove(downloads_path + '/' + item)
+            shutil.move(downloads_path + '/' + item, text_file_path + '/' + item)
 
     for i in image_extensions:
         if(item.endswith(i)):
-            os.remove(downloads_path + '/' + item)
+            shutil.move(downloads_path + '/' + item, image_file_path + '/' + item)
 
     for i in audio_extensions:
         if(item.endswith(i)):
-            os.remove(downloads_path + '/' + item)
+            shutil.move(downloads_path + '/' + item, audio_file_path + '/' + item)
 
     for i in video_extensions:
         if(item.endswith(i)):
-            os.remove(downloads_path + '/' + item)
+            shutil.move(downloads_path + '/' + item, video_file_path + '/' + item)
 
+    # deleting executable files. i.e. installers
     for i in executable_extensions:
         if(item.endswith(i)):
             os.remove(downloads_path + '/' + item)
     
+    # deleting compressed files
     for i in compressed_extensions:
         if(item.endswith(i)):
             os.remove(downloads_path + '/' + item)
